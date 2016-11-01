c2b293bf9b97ed98bedc82694960bd8f3702d41a
Adam Cherochak <genericCog@users.noreply.github.com>
2016-11-01 14:38:21 -0700

    complete code for project

---
+++ 04-rollercoaster.py
@@ -0,0 +1,7 @@
+age = eval(input("How old are you?"))
+height = eval(input("How tall are you? (4.3 for four foot three inches, etc.)"))
+
+if age >= 9 and height >= 4.0:
+    print("you can get on the ride!")
+else:
+    print("you can't get on the ride.")
